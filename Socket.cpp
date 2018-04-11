3cabaa5eddff4df7cd4c93287f09ebcaefe62a32
Miroslav Baudys <12230741+miroslavbaudys@users.noreply.github.com>
2018-04-11 16:23:05 +0200

    Bundle SQLite amalgamation and add socket logging

---
+++ Socket.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Miroslav Baudys on 11/04/2018.
+//
+
+#include "Socket.h"
+#include "Logger.h"
+
+Socket::Socket(boost::asio::io_service& service, boost::asio::ip::tcp::socket socket) :
+        m_service(service),
+        m_socket(std::move(socket))
+{
+    Log.debug("Socket create\n");
+}
+
+Socket::~Socket()
+{
+    Log.debug("Socket destroy\n");
+}
